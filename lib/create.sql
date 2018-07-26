a48c010c68e70ea0b7b232968c7a4489dc22db0c
Nate Miller <natekmiller96@gmail.com>
2018-07-26 03:41:44 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,5 +1,3 @@
-CREATE TABLE groceries (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER, aisle INTEGER);
- 
-INSERT INTO groceries VALUES (1, "Apples", 2, 1);
-INSERT INTO groceries VALUES (2, "Oranges", 4, 2);
-INSERT INTO groceries VALUES(3, "Peaches", 6, 3);
+CREATE TABLE project (id INTEGER PRIMARY KEY, title TEXT, , category TEXT, funding_goal INTEGER, start_date INTEGER, end_date INTEGER);
+CREATE TABLE user (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
+CREATE TABLE pledge (id INTEGER PRIMARY KEY, amount INTEGER, user_id INTEGER, project_id INTEGER);
